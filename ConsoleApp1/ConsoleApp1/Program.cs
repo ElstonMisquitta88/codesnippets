75b1b64a3631b7e19d40734ebbed3d1e0175d12e
Elston Misquitta <elston007@gmail.com>
2026-05-23 10:37:42 +0530

    Code Changes

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿
+// (1) Reverse a string using StringBuilder
+// Logic
+// Start from:last character
+// Move backwards.
+
+using System.Text;
+string input = "hello";
+StringBuilder reversed = new StringBuilder();
+for (int i = input.Length - 1; i >= 0; i--)
+{
+    reversed.Append(input[i]);
+}
+Console.WriteLine(reversed.ToString());
